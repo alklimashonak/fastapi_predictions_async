060cf7b18fc5bfae41a8d07cc7debfb4ca89016c
alkli <alklimashonak@gmail.com>
2023-07-10 21:22:01 +0200

    connect alembic to test db, refactor migrations

--- src/predictions/models.py
+++ src/predictions/models.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import Integer, ForeignKey, UUID
+from sqlalchemy import Integer, ForeignKey, UUID, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.database import Base
@@ -8,6 +8,9 @@ from src.database import Base
 
 class Prediction(Base):
     __tablename__ = 'predictions'
+    __table_args__ = (
+        UniqueConstraint('match_id', 'user_id', name='uix_predictions_match_id_user_id'),
+    )
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     home_goals: Mapped[int] = mapped_column(Integer, nullable=True, default=None)
