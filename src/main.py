317c347305c9eeeb0022d520269342cec17acbcf
alkli <alklimashonak@gmail.com>
2023-07-29 00:33:40 +0200

    add CORSMiddleware

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from os import path
 
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from src.auth.router import router as auth_router
 from src.events.router import router as event_router
@@ -18,6 +19,13 @@ app.include_router(auth_router, prefix='/auth', tags=['Auth'])
 app.include_router(event_router, prefix='/events', tags=['Events'])
 app.include_router(prediction_router, prefix='', tags=['Predictions'])
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
